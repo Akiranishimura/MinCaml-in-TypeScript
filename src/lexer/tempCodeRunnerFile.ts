afa9c02963dfa1258d1c95d1aa6cdc83e9ccb2bb
Akiranishimura <nissi20ch@gmail.com>
2025-12-31 21:25:39 +0900

    delete temp file

--- src/lexer/tempCodeRunnerFile.ts
+++
@@ -1,8 +0,0 @@
-	test("変数宣言をトークン化できる", () => {
-		const tokens = tokenize("let x = 1");
-		expect(tokens).toEqual([
-			{ type: "LET" },
-			{ type: "IDENT", value: "x" },
-			{ type: "EQ" },
-			{ type: "NUMBER", value: 1 },
-			{ type: "EOF" },
